dc6086e8b5b208311074c98b4007bdacb7ef26df
Akhill-bee <akhilbollapragada@gmail.com>
2026-07-23 18:30:20 -0500

    setup for deployment

--- app/main.py
+++ app/main.py
@@ -85,5 +85,6 @@ def taylor():
     ))
 
 if __name__ == "__main__":
+    # Render assigns a dynamic port via environment variables
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
